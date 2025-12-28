43c4ecefc7978d6e831ca6097b78965c46d4b2ea
One <ford-HUB@gmail.com>
2025-12-28 22:37:00 +0800

    fixed the endpoint of api in frontend connection backend

--- frontend/api/_base.ts
+++ frontend/api/_base.ts
@@ -1,8 +1,6 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-    baseURL: process.env.NODE_ENV === 'production'
-        ? 'https://fatima-as-it-was-given.onrender.com'
-        : 'http://localhost:8000/api',
+    baseURL: 'https://fatima-as-it-was-given.onrender.com',
     withCredentials: true
 })
