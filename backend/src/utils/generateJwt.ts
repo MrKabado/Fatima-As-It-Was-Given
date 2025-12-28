c3acedcfbe891759d941ee534e37c82632695b66
One <ford-HUB@gmail.com>
2025-12-28 21:48:51 +0800

    modify the jwt config to lax for production

--- backend/src/utils/generateJwt.ts
+++ backend/src/utils/generateJwt.ts
@@ -16,7 +16,7 @@ export const generateJwt = (payload: IPayload, res: Response) => {
         res.cookie('jwt', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'lax',
             maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
         })
 
